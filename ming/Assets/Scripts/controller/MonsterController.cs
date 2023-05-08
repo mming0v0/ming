c4c763c66ca5c6fd94dc00a3c8a3831dee5de5a0
mming0v0 <minji10221004@gmail.com>
2023-05-08 13:04:05 +0900

    적 생성

--- ming/Assets/Scripts/controller/MonsterController.cs
+++ ming/Assets/Scripts/controller/MonsterController.cs
@@ -14,6 +14,4 @@ public class MonsterController : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
-         
-
 }
--- ming/Assets/Scripts/controller/ProjectMove.cs
+++ ming/Assets/Scripts/controller/ProjectMove.cs
@@ -8,13 +8,27 @@ public class ProjectMove : MonoBehaviour
     public void OnCollisionEnter(Collision collision)
     {
 
-        if (collision.gameObject.name == "wall1")
+        if (collision.gameObject.tag == "wall1")
         {
             Destroy(this.gameObject);
         }
-        if (collision.gameObject.name == "Monster")
+        if (collision.gameObject.tag == "Monster")
         {
             collision.gameObject.GetComponent<MonsterController>().Damanged(1);
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Wall")
+        {
+            Destroy(this.gameObject);
+        }
+        if (other.gameObject.tag == "Monster")
+        {
+            other.gameObject.GetComponent<MonsterController>().Damanged(1);
             Destroy(this.gameObject);
         }
     }
